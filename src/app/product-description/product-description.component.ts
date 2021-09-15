8142243a6273264fa21a2c494d3049ea904e4b23
vuducthien <thienducvu@gmail.com>
2021-09-15 19:22:26 +0700

    Completed module

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'app/product.service';
 
 @Component({
   selector: 'app-product-description',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDescriptionComponent implements OnInit {
 
-  constructor() { }
+  albumInfo: any;
+
+  constructor(private _productService: ProductService) { }
 
   ngOnInit() {
+    this._productService.getAlbum(1).subscribe(response => this.albumInfo = response);
+    // setTimeout(() =>
+    // this._productService.getAlbum(1).subscribe(response => this.albumInfo = response), 1000
+    // )
   }
 
 }
---
+++ src/app/product.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Http, Response} from '@angular/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable()
+export class ProductService {
+
+  _albumUrl: string = "../assets/album.json";
+
+  constructor(private _http: Http) { }
+
+  getAlbum(id: number) {
+    return this._http.get(this._albumUrl).pipe(
+      map(response => response.json())
+    )
+  }
+
+}
